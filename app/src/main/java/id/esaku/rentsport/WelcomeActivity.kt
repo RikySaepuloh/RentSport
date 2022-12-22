eaad57754892bfab2b54e2ecf43c9af76a54c44e
Riky <dikysaepuloh@gmail.com>
2022-12-22 16:54:39 +0700

    initial commit

---
+++ app/src/main/java/id/esaku/rentsport/WelcomeActivity.kt
@@ -0,0 +1,11 @@
+package id.esaku.rentsport
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
