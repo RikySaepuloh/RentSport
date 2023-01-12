915205ff0c311cb191536acd05d4639491a2fbc9
aciw <riskyy@student.telkomuniversity.ac.id>
2023-01-12 14:03:35 +0700

    menambahkan layout profile

--- app/src/main/java/id/esaku/rentsport/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/id/esaku/rentsport/ui/notifications/NotificationsFragment.kt
@@ -5,15 +5,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import id.esaku.rentsport.ui.login.EditProfileActivity
 import id.esaku.rentsport.Preferences
 import id.esaku.rentsport.ui.login.LoginActivity
 import id.esaku.rentsport.TicketActivity
+import id.esaku.rentsport.data.source.local.entity.UserEntity
 import id.esaku.rentsport.databinding.FragmentNotificationsBinding
+import id.esaku.rentsport.ui.login.UserViewModel
 
 class NotificationsFragment : Fragment() {
-
+    private lateinit var mUserViewModel: UserViewModel
     private var _binding: FragmentNotificationsBinding? = null
 
     // This property is only valid between onCreateView and
@@ -40,6 +44,15 @@ class NotificationsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         preferences.setPreferences(requireContext())
+        mUserViewModel = ViewModelProvider(this)[UserViewModel::class.java]
+        mUserViewModel.getUser(preferences.getIdDevice())
+        mUserViewModel.user.observe(this) {
+            if (it != null) {
+                setData(it)
+            } else {
+                Toast.makeText(requireContext(), "Terjadi kesalahan", Toast.LENGTH_LONG).show()
+            }
+        }
         binding.btnKeluar.setOnClickListener{
             requireActivity().finishAffinity()
             preferences.preferencesLogout()
@@ -58,5 +71,12 @@ class NotificationsFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setData(entity: UserEntity){
+        binding.tvUsername.setText(entity.username)
+        binding.tvNamaLengkap.setText(entity.nama)
+        binding.tvEmail.setText(entity.email)
+        binding.tvAlamat.setText(entity.alamat)
     }
 }
